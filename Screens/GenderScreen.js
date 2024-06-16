3fdfa68dbf92589e8ccd37fb59bb2ad06310c8da
piyush <gupta.piyush1048@gmail.com>
2024-06-16 17:46:17 +0530

    type and gender Screen

--- Screens/GenderScreen.js
+++ Screens/GenderScreen.js
@@ -1,11 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,TouchableOpacity,Image} from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useState } from 'react'
 
 const GenderScreen = () => {
+
+  const navigation=useNavigation();
+  const [gender,setGender] = useState('')
   return (
-    <View>
-      <Text>GenderScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{flexDirection:'row',alignItems:'center',marginTop:70 , marginLeft:15}}>
+        <View style={{borderRadius:22,borderWidth:2, height:44,width:44}}>
+          <MaterialCommunityIcons name='newspaper-variant-outline' style={{textAlign:'center',marginTop:3}} size={34} color='black'/>
+        </View>
+
+        <Image style={{ height: 100, width: 100 }}
+          source={{ uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png' }}>
+
+        </Image>
+      </View>
+      <View style={{marginTop:-10}}>
+        <Text style={{fontSize:30,fontWeight:'bold',color:'black',marginLeft:15}} >Which Gender Describes You the Best ?</Text>
+        <Text style={{ marginLeft:15, marginRight:15,marginTop:15}}>This is Piyush Dating App and This is Going to be a Thing in Comming Years</Text>
+      </View>
+      <View style={{marginTop:15}}>
+        <View style={{marginLeft:17 , flexDirection:'row' , justifyContent:'space-between',marginRight:15}}>
+        <Text style={{fontSize:20,color:'black'}}>Men</Text>
+        <MaterialCommunityIcons onPress={()=>setGender('Men')} name='circle' size={24} color={gender==='Men'?'grey':'black'}/>
+        </View>
+
+        <View style={{marginLeft:17 ,marginTop:15, flexDirection:'row' , justifyContent:'space-between',marginRight:15}}>
+        <Text style={{fontSize:20,color:'black'}}>Women</Text>
+        <MaterialCommunityIcons onPress={()=>setGender('Women')} name='circle' size={24} color={gender==='Women'?'grey':'black'}/>
+        </View>
+
+        
+
+        <View style={{marginLeft:17 ,marginTop:15, flexDirection:'row' , justifyContent:'space-between',marginRight:15}}>
+        <Text style={{fontSize:20,color:'black'}}>TransGender</Text>
+        <MaterialCommunityIcons onPress={()=>setGender('trans')} name='circle' size={24} color={gender==='trans'?'grey':'black'}/>
+        </View>
+
+
+
+        
+
+        <TouchableOpacity onPress={()=>navigation.navigate('type')} style={{marginTop:20}}>
+        <MaterialCommunityIcons 
+
+style={{textAlign:'center',
+  marginTop:14,marginLeft:'auto',marginRight:15,}}
+   name='arrow-right-circle'
+    size={50} 
+color={'black'}/>
+
+        </TouchableOpacity>
+      </View>
+
+
+    </SafeAreaView>
   )
 }
 
--- Screens/TypeScreen.js
+++ Screens/TypeScreen.js
@@ -1,13 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image,TouchableOpacity,SafeAreaView } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const TypeScreen = () => {
+  const navigation=useNavigation();
+  const [type,setType] = useState('')
   return (
-    <View>
-      <Text>TypeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={{flexDirection:'row',alignItems:'center',marginTop:70 , marginLeft:15}}>
+        <View style={{borderRadius:22,borderWidth:2, height:44,width:44}}>
+          <MaterialCommunityIcons name='newspaper-variant-outline' style={{textAlign:'center',marginTop:3}} size={34} color='black'/>
+        </View>
+
+        <Image style={{ height: 100, width: 100 }}
+          source={{ uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png' }}>
+
+        </Image>
+      </View>
+      <View style={{marginTop:-10}}>
+        <Text style={{fontSize:30,fontWeight:'bold',color:'black',marginLeft:15}} >Whats Your Sexuality ?</Text>
+        <Text style={{ marginLeft:15, marginRight:15,marginTop:15}}>Sexuality is very Important part of ones life .Everybody has the right to choose their Sexuality</Text>
+      </View>
+      <View style={{marginTop:15}}>
+        <View style={{marginLeft:17 , flexDirection:'row' , justifyContent:'space-between',marginRight:15}}>
+        <Text style={{fontSize:20,color:'black'}}>Straight</Text>
+        <MaterialCommunityIcons onPress={()=>setType('st')} name='circle' size={24} color={type==='st'?'grey':'black'}/>
+        </View>
+
+
+        <View style={{marginLeft:17 , flexDirection:'row' , justifyContent:'space-between',marginRight:15,marginTop:15}}>
+        <Text style={{fontSize:20,color:'black'}}>Lesbian</Text>
+        <MaterialCommunityIcons onPress={()=>setType('ls')} name='circle' size={24} color={type==='ls'?'grey':'black'}/>
+        </View>
+
+        <View style={{marginLeft:17 , flexDirection:'row' , justifyContent:'space-between',marginRight:15,marginTop:15}}>
+        <Text style={{fontSize:20,color:'black'}}>Gay</Text>
+        <MaterialCommunityIcons onPress={()=>setType('gy')} name='circle' size={24} color={type==='gy'?'grey':'black'}/>
+        </View>
+
+        <View style={{marginLeft:17 , flexDirection:'row' , justifyContent:'space-between',marginRight:15,marginTop:15}}>
+        <Text style={{fontSize:20,color:'black'}}>BiSexual</Text>
+        <MaterialCommunityIcons onPress={()=>setType('bs')} name='circle' size={24} color={type==='bs'?'grey':'black'}/>
+        </View>
+
+       
+
+        <TouchableOpacity onPress={()=>navigation.navigate('dating')} style={{marginTop:20}}>
+        <MaterialCommunityIcons 
+
+style={{textAlign:'center',
+  marginTop:14,marginLeft:'auto',marginRight:15,}}
+   name='arrow-right-circle'
+    size={50} 
+color={'black'}/>
+
+        </TouchableOpacity>
+      </View>
+
+
+    </SafeAreaView>
+    )
+  }
 
 export default TypeScreen
 
